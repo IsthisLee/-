3bb34c9c3a3b8054eff7a1f2d8db939f1b183cf4
IsthisLee <rjsgmldnwn@gmail.com>
2022-03-26 19:51:55 +0900

    feat(game): add function of calculating the result of a numerical comparison

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 //게임 함수
 function game() {
-  let randNum, randNums, userNums;
-  randNum = getRandNum(); // 난수 생성
-  randNums = getComNums(randNum); //컴퓨터의 3자리 숫자 생성
-  userNums = getUserNums(); //사용자의 3자리 숫자 받기
+  let randNum, comNums, userNums, compareResultArr;
 
-  console.log(randNums, userNums);
+  randNum = getRandNum(); //난수 생성
+  comNums = getComNums(randNum); //컴퓨터의 3자리 숫자 생성
+  userNums = getUserNums(); //사용자의 3자리 숫자 받기
+  compareResultArr = compareNums(comNums, userNums); //두 숫자의 각 자리 비교 결과 배열
+
+  console.log(comNums, userNums, compareResultArr);
 }
 
 //1~9 난수 생성 함수
@@ -45,4 +47,40 @@ function getUserNums() {
   } while (userNums.length !== 3);
   console.log("사용자의 숫자 : ", userNums, typeof userNums);
   return userNums;
+}
+
+//조건. 같은 자리인 경우(스트라이크) true 반환 함수
+function checkStrike(round, comNums, userNum) {
+  if (comNums[round] === userNum) {
+    return true;
+  } else return false;
+}
+
+//조건. 다른 자리인 경우(볼) true 반환 함수
+function checkBall(comNums, userNum) {
+  if (comNums.includes(userNum)) {
+    return true;
+  } else return false;
+}
+
+//사용자와 컴퓨터의 숫자 비교 함수
+function compareNums(standardNums, targetNums) {
+  let strike,
+    ball,
+    result = [];
+
+  for (let i = 0; i < 3; i++) {
+    strike = checkStrike(i, standardNums, targetNums[i]);
+    ball = checkBall(standardNums, targetNums[i]);
+    if (strike) {
+      result.push("스트라이크");
+      continue;
+    } else if (ball) {
+      result.push("볼");
+    } else {
+      result.push("낫싱");
+    }
+  }
+  console.log("비교 결과 : ", result);
+  return result;
 }
