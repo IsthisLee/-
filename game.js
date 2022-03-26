6118f6969d49f799fa42257df91b55f742fdcd33
IsthisLee <rjsgmldnwn@gmail.com>
2022-03-26 21:04:01 +0900

    docs: edit annotation and Readme.md's featlist

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
 //게임 함수
+//1.게임 시작 버튼 클릭 시 game 함수 호출
 function game(nums) {
   let randNum, comNums, userNums, compareResultArr, checkStrike, checkEnd;
 
-  randNum = getRandNum(); //난수 생성
+  randNum = getRandNum(); //2. 난수 생성
   //컴퓨터의 숫자가 없는 경우(첫 라운드인 경우)
   if (!nums) {
-    comNums = getComNums(randNum); //컴퓨터의 3자리 숫자 생성
+    comNums = getComNums(randNum); //3. 컴퓨터의 3자리 숫자 생성
   } else comNums = nums; // 첫 라운드가 아닌 경우 기존 숫자
-  userNums = getUserNums(); //사용자의 3자리 숫자 받기
+  userNums = getUserNums(); //4. 사용자의 3자리 숫자 받기
   if (userNums === null) return; //사용자가 취소 시 게임 종료
-  compareResultArr = compareNums(comNums, userNums); //두 숫자의 각 자리 비교 결과 배열
-  giveHint(compareResultArr); //사용자에게 라운드 결과(힌트) 제공
-  checkStrike = checkThreeStrike(compareResultArr); //쓰리 스트라이크 여부 확인
-  checkEnd = checkGame(comNums, checkStrike); //게임 결과에 따라 종료 또는 재시작
-  //게임 종료 시 새로 시작 여부 묻기
+  compareResultArr = compareNums(comNums, userNums); //5. 두 숫자의 각 자리 비교 결과 배열
+  giveHint(compareResultArr); //6. 사용자에게 라운드 결과(힌트) 제공
+  checkStrike = checkThreeStrike(compareResultArr); //7. 쓰리 스트라이크 여부 확인
+  checkEnd = checkGame(comNums, checkStrike); //8. 게임 결과에 따라 종료 또는 재시작
+  //9. 게임 종료 시 새로 시작 여부 묻기
   if (checkEnd) {
     answerRestart();
   }
