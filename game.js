0d45aec67c66e422e1dc9e3a4e9d32a93857ac21
IsthisLee <rjsgmldnwn@gmail.com>
2022-03-26 19:33:11 +0900

    feat(game): add random number making function

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
 function game() {
-  console.log("start game");
+  let randNum = getRandNum();
+  console.log(randNum);
+}
+
+//1~9 난수 생성 함수
+function getRandNum() {
+  let randNum = 0;
+
+  do {
+    randNum = Math.floor(Math.random() * 10);
+  } while (!randNum);
+  return randNum;
 }
